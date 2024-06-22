599ea092bf713923f787ec19846281530d99cc64
Fernando <you@example.com>
2024-06-22 19:11:24 -0300

    correção da api

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-function buscarCep() {
+async function buscarCep() {
     const cep = document.getElementById("cep").value;
+    const latitude = document.getElementById("latitude").value;
+    const longitude = document.getElementById("longitude").value;
     
     try {
-        const resposta = fetch(`viacep.com.br/ws/01001000/json/`)
-        console.log(resposta)
-        location.reload(true)
+        const response =  await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const data = await response.json();
+        console.log(data);
+
+        document.getElementById("rua").textContent =data.logradouro;
+        document.getElementById("bairro").textContent=data.bairro;
+        document.getElementById("uf").textContent =data.uf;
         
     } catch (error) {
         console.log(error)
         alert(error.message)
     }
-     
+
+    try {
+        const resp =  await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
+        const date = await resp.json();
+        console.log(date);
+
+        document.getElementById("resp").textContent="";
+        for (let index = 0; index < date.hourly.temperature_2m.length; index++) {
+            document.getElementById("resp").innerHTML += `<div>${date.hourly.temperature_2m[index]}</div>`
+            
+        }
+        
+    } catch (error) {
+        console.log(error)
+        alert(error.message)
+    }
+    
 
 }
